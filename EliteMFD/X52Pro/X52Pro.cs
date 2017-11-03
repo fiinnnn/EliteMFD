3c46efd5433c2ad156d088e5f23fdd05b694494e
Finn <finnvos34@protonmail.ch>
2017-11-03 12:02:13 +0100

    Added a way to find connected X52

---
+++ EliteMFD/X52Pro/X52Pro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EliteMFD.X52Pro
+{
+    class X52Pro
+    {
+        private IntPtr device;
+
+        public IntPtr Device
+        {
+            get { return device; }
+        }
+
+        public X52Pro(IntPtr device)
+        {
+            this.device = device;
+        }
+    }
+}
---
+++ EliteMFD/X52Pro/X52ProManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using DirectOutputCSharpWrapper;
+
+namespace EliteMFD.X52Pro
+{
+    class X52ProManager
+    {
+        private List<X52Pro> attachedDevices;
+        private DirectOutput directOutput;
+
+        public X52ProManager(string appName)
+        {
+            directOutput = new DirectOutput();
+            directOutput.Initialize(appName);
+        }
+
+        public void FindDevices()
+        {
+            directOutput.Enumerate((IntPtr device, IntPtr context) => { attachedDevices.Add(new X52Pro(device)); });
+        }
+    }
+}
