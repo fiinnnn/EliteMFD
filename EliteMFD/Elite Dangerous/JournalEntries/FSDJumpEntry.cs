c022b7868ba4a6a6522c59496a59de9e4f3e4fe7
Finn <finnvos34@protonmail.ch>
2017-11-07 20:49:09 +0100

    Added some of the journal events
    Events added are:
    LoadGame, FSDJump and FuelScoop

---
+++ EliteMFD/Elite Dangerous/JournalEntries/FSDJumpEntry.cs
@@ -0,0 +1,26 @@
+﻿using Vector3D;
+using Newtonsoft.Json.Linq;
+
+namespace EliteMFD.EliteDangerous
+{
+    class FSDJumpEntry : JournalEntry
+    {
+        #region parameters
+        public string StarSystem { get; set; }
+        public Vector StarPos { get; set; }
+        public double JumpDist { get; set; }
+        public double FuelUsed { get; set; }
+        public double FuelLevel { get; set; }
+        #endregion
+
+        public FSDJumpEntry(JObject entry) : base(entry)
+        {
+            StarSystem = entry.Value<string>("StarSystem");
+            JArray coords = entry.Value<JArray>("StarPos");
+            StarPos = new Vector(coords[0].Value<float>(), coords[1].Value<float>(), coords[2].Value<float>());
+            JumpDist = entry.Value<double>("JumpDist");
+            FuelUsed = entry.Value<double>("FuelUsed");
+            FuelLevel = entry.Value<double>("FuelLevel");
+        }
+    }
+}
---
+++ EliteMFD/Elite Dangerous/JournalEntries/FuelScoopEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace EliteMFD.EliteDangerous
+{
+    class FuelScoopEntry : JournalEntry
+    {
+        #region parameters
+        public double Scooped { get; set; }
+        public double Total { get; set; }
+        #endregion
+
+        public FuelScoopEntry(JObject entry) : base(entry)
+        {
+            Scooped = entry.Value<double>("Scooped");
+            Total = entry.Value<double>("Total");
+        }
+    }
+}
