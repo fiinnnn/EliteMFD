8ee2247de5efb29d624d9da490eb2bb863759ff5
Finn <finnvos34@protonmail.ch>
2017-11-07 20:51:41 +0100

    Added a JournalParser class

---
+++ EliteMFD/Elite Dangerous/JournalParser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+
+namespace EliteMFD.EliteDangerous
+{
+    class JournalParser
+    {
+        public JournalParser()
+        {
+        }
+
+        public JournalEntry ParseLine(string line)
+        {
+            JObject entry = JObject.Parse(line);
+
+            switch (entry.Value<string>("event"))
+            {
+                default:
+                    return new JournalEntry(entry);
+
+                case "LoadGame":
+                    return new LoadGameEntry(entry);
+
+                case "FSDJump":
+                    return new FSDJumpEntry(entry);
+
+                case "FuelScoop":
+                    return new FuelScoopEntry(entry);
+            }
+        }
+    }
+}
